276e69439e6a99904c82c1ebb5e9e1cf0d7d0b9b
Emilse Arias <emilsearias@gmail.com>
2022-09-21 10:47:49 -0300

    agregamos tareas al arreglo de tareas y limpiamos el value del input tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import ListaTarea from "./ListaTarea";
 
 const FormularioTarea = () => {
+  // aqui va la logica
+  const [tarea, setTarea] = useState("");
+  const [arregloTareas, setArregloTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("desde handleSubmit");
+    setArregloTareas([...arregloTareas, tarea]);
+    setTarea('');
+  };
+
   return (
     <div>
-      <Form>
-        <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-        {/* form.control es el input */}
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex">
+          {/* form.control es el input */}
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
           <Button variant="primary" type="submit">
             Enviar
           </Button>
